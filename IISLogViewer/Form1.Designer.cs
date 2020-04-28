14dbb64431025c281da01f4d4900245e9ba98ba7
友光 草場 <tomokusaba@tomokusaba.onmicrosoft.com>
2020-04-29 07:50:44 +0900

    プロジェクト ファイルを追加します。

---
+++ IISLogViewer/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace IISLogViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowDrop = true;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 71);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(804, 382);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.DragDrop += new System.Windows.Forms.DragEventHandler(this.dataGridView1_DragDrop);
+            this.dataGridView1.DragEnter += new System.Windows.Forms.DragEventHandler(this.dataGridView1_DragEnter);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(28, 29);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(205, 20);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(266, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(171, 19);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(494, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 18);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "絞り込み";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ IISLogViewer/Form1.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IISLogViewer
+{
+    public partial class Form1 : Form
+    {
+
+        DataTable dt = null;
+        DataSet ds = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void dataGridView1_DragDrop(object sender, DragEventArgs e)
+        {
+            dt.Clear();
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            foreach(string file in files)
+            {
+                Read(file);
+            }
+
+            dataGridView1.DataSource = dt;
+        }
+
+        private void Read(string path)
+        {
+            using (StreamReader sr = new StreamReader(@path, Encoding.GetEncoding("Shift_JIS")))
+            {
+
+                string log = sr.ReadToEnd();
+                log = log.Replace('[', '"');
+                log = log.Replace(']', '"');
+                byte[] data = System.Text.Encoding.GetEncoding("Shift_JIS").GetBytes(log);
+
+                MemoryStream st = new MemoryStream(data);
+
+                using (TextFieldParser parser = new TextFieldParser(st))
+                {
+
+                    parser.TextFieldType = FieldType.Delimited;
+                    parser.SetDelimiters(" ");
+                    parser.CommentTokens = new String[] { "#" };
+
+
+
+                    string[] line = new string[7];
+                    while (!parser.EndOfData)
+                    {
+                        try
+                        {
+                            line = parser.ReadFields();
+                        }
+                        catch { }
+
+                        EncLine(line);
+                    }
+                }
+            }
+        }
+
+        private void EncLine(string[] line)
+        {
+            DataGridViewRow row = new DataGridViewRow();
+            row.CreateCells(dataGridView1);
+
+            DataRow dr = dt.NewRow();
+
+            dr[0] = line[0];
+            dr[1] = line[1];
+            dr[2] = line[2];
+            dr[3] = line[3];
+            dr[4] = line[4];
+            dr[5] = line[5];
+            dr[6] = line[6];
+
+
+            dt.Rows.Add(dr);
+        }
+
+        private void CrerateDateGrid()
+        {
+            dataGridView1.Columns.Clear();
+            dataGridView1.Rows.Clear();
+
+            dt = new DataTable("table");
+
+            dt.Columns.Add("アクセス元");
+            dt.Columns.Add("Cユーザ名");
+            dt.Columns.Add("認証ユーザ名");
+            dt.Columns.Add("時刻");
+            dt.Columns.Add("リソース");
+            dt.Columns.Add("ステータス");
+            dt.Columns.Add("転送量");
+
+            comboBox1.Items.Add("アクセス元");
+            comboBox1.Items.Add("Cユーザ名");
+            comboBox1.Items.Add("認証ユーザ名");
+            comboBox1.Items.Add("時刻");
+            comboBox1.Items.Add("リソース");
+            comboBox1.Items.Add("ステータス");
+            comboBox1.Items.Add("転送量");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CrerateDateGrid();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataRow[] rows = dt.AsEnumerable().Where(row => row.Field<string>(comboBox1.SelectedItem.ToString()) == textBox1.Text).ToArray();
+
+            if (rows.Length == 0)
+            {
+                MessageBox.Show("該当するデータがありません");
+                return;
+            }
+
+            dt = rows.CopyToDataTable();
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
